918340be927d1f174d8b82dbed9eb055984b1276
Drew <drew.m.white51@gmail.com>
2020-11-02 15:05:26 -0600

    Sum of numbers update.

--- src/main/java/Chapter4Package/SumOfNumbers.java
+++ src/main/java/Chapter4Package/SumOfNumbers.java
@@ -5,12 +5,41 @@
  */
 package Chapter4Package;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Drew
  */
+
+// PROBLEM DISCRIPTION 
+// Write a program that asks the user for a positive nonzero integer value
+// This program should use  aloop to get the sum of all integers from 1 up to the number entered 
+// For example if the user enters 50, the loop will find the sum of 1,2,3,4...5
+
 public class SumOfNumbers {
+   
     
+    public static void sumOfNumbers() {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter a positive integer.");
+        int userInput = keyboard.nextInt();
+        
+        int x;
+        int total = 0;
+        
+        for (x=1; x <= userInput; x++) {
+            total += x;
+        }
+        
+        System.out.println(total);
+        
+    }
     
+    public static void main(String[] args) {
+        sumOfNumbers();
+        
+    }
     
 }
